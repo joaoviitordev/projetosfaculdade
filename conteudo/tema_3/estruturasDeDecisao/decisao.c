b26ee43c92669c5db63de5db833840b8874f6f23
joaoviitordev <joaovitorgomesdefaria@gmail.com>
2025-12-12 09:28:15 -0300

    estrutura de decisão if

--- conteudo/tema_3/estruturasDeDecisao/decisao.c
+++ conteudo/tema_3/estruturasDeDecisao/decisao.c
@@ -3,13 +3,26 @@
 // Iniciando as Estruturas de Decisão em C
 
 int main () {
-    int a = 10;
+    int a = 20;
     int b = 20;
 
-    if (a == b ) {
-        printf ("A variável A é igual a variável B\n");
+    if (a == b) {
+        printf ("A variável A é igual a variável B.\n");
     } else {
-        printf("A variável A é diferente da variável B\n");
+        printf("A variável A é diferente da variável B.\n");
     }
+
+    int num = 40;
+
+    if (num % 2 == 0) {
+        printf ("Esse número é par.");
+    }
+
+    return 0;
 }
 
+// O que fica entre os parenteses do "if" é uma expressão lógica (Condição) que pode ser verdadeira ou falsa.
+// Se a condição for verdadeira, o bloco de código dentro das chaves do "if" será executado.
+// Se a condição for falsa, o bloco de código dentro das chaves do "else" será executado.
+// Note que o "else" é opcional. Podemos usar apenas o "if" se desejarmos.
+// if pode ser traduzido como "se" e else como "senão".
