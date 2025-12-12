9f10e9ef0d61197cc241fb752660b0c35b6aa959
joaoviitordev <joaovitorgomesdefaria@gmail.com>
2025-12-12 09:02:56 -0300

    organização de pastas

---
+++ conteudo/tema_3/estruturasDeDecisao/decisao.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Iniciando as Estruturas de Decisão em C
+
+int main () {
+    int a = 10;
+    int b = 20;
+
+    if (a == b ) {
+        printf ("A variável A é igual a variável B\n");
+    } else {
+        printf("A variável A é diferente da variável B\n");
+    }
+}
+
