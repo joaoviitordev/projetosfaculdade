af38ac809a05a5e2047c8e192958284c0bc792e8
joaoviitordev <joaovitorgomesdefaria@gmail.com>
2025-12-18 13:51:47 -0300

    switch

---
+++ conteudo/tema_3/switch/switch.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// Exemplo de uso do switch em C
+
+int main () {
+    int dia = 0;
+
+    switch (dia) {
+        case 1:
+            printf("Segunda-feira\n");
+            break;
+        case 2:
+            printf("Terça-feira\n");
+            break;
+        case 3:
+            printf("Quarta-feira\n");
+            break;
+        case 4:
+            printf("Quinta-feira\n");
+            break;
+        case 5:
+            printf("Sexta-feira\n");
+            break;
+        case 6:
+            printf("Sábado\n");
+            break;
+        case 7:
+            printf("Domingo\n");
+            break;
+        default:
+            printf("Dia inválido\n");
+    }
+
+    return 0;
+}
